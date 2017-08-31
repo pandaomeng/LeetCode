d2a72714cf89086ecfe47ed79387cfe74775c0df
pandaomeng <pandaomeng@corp-ci.com>
2017-08-31 10:02:26 +0800

    add-two-number

--- Python/add-two-number.py
+++ Python/add-two-number.py
@@ -1,15 +1,17 @@
-import copy
-
 # Definition for singly-linked list.
+
+
 class ListNode(object):
   def __init__(self, x):
     self.val = x
     self.next = None
+
   def print(self):
     temp = self
     while temp:
-      print(temp.val,'-> ',end='')
+      print(temp.val, '->', end=' ')
       temp = temp.next
+
 
 class Solution(object):
   def addTwoNumbers(self, l1, l2):
@@ -21,10 +23,10 @@ class Solution(object):
     ret = ListNode(0)
     cur = ret
     add = 0
-    
+
     while l1 or l2 or add:
       val = (l1.val if l1 else 0) + (l2.val if l2 else 0) + add
-      add = val / 10
+      add = val // 10
       cur.next = ListNode(val % 10)
       cur = cur.next
       l1 = l1.next if l1 else None
@@ -32,21 +34,24 @@ class Solution(object):
 
     return ret.next
 
+
 if __name__ == '__main__':
   solution = Solution()
-  node1 = ListNode(2)
-  node2 = ListNode(4)
-  node3 = ListNode(3)
-  node1.next = copy.copy(node2)
-  node2.next = copy.copy(node3)
-  l1 = copy.copy(node1)
-
-  node1 = ListNode(5)
+  node1 = ListNode(1)
   node2 = ListNode(6)
-  node3 = ListNode(4)
-  node1.next = copy.copy(node2)
-  node2.next = copy.copy(node3)
-  l2 = copy.copy(node1)
+  node3 = ListNode(3)
+  node2.next = node3
+  node1.next = node2
+  l1 = node1
 
-  print(l1.print())
+  node1 = ListNode(7)
+  node2 = ListNode(2)
+  node3 = ListNode(9)
+  node4 = ListNode(9)
+  node3.next = node4
+  node2.next = node3
+  node1.next = node2
+  l2 = node1
+
   ret = solution.addTwoNumbers(l1, l2)
+  ret.print()
