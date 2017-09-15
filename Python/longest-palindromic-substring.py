56c645ebd307080fa406e9bfd40ffd6abc928a56
pandaomeng <pandaomeng@corp-ci.com>
2017-09-15 12:02:06 +0800

    ac

--- Python/longest-palindromic-substring.py
+++ Python/longest-palindromic-substring.py
@@ -4,23 +4,40 @@ class Solution(object):
     :type s: str
     :rtype: str
     """
-    result = ''
-    maxLength = 1
+    result = s[0]
+    maxLength = 0
     # 找中心点
     for i in range(len(s)):
-    	for j in range(min(i, len(s)-1-i)):
-    		if (s[i-j] == s[i+j]):
-    			
+      # 奇数个字符的回文数字
+      currentLength = 1
+      currentResult = s[i]
+      for j in range(1, min(i - 0, len(s) - 1 - i) + 1):
+        if (s[i - j] == s[i + j]):
+          currentLength += 2
+          currentResult = s[i - j] + currentResult + s[i + j]
+          if currentLength > maxLength:
+            maxLength = currentLength
+            result = currentResult
+        else:
+          break
 
+      # 偶数个字符的回文数字
+      currentLength = 0
+      currentResult = ''
+      # 偶回文数，中心向右偏0.5字节，计算左边遍历长度的时候+1
+      for j in range(1, min(i - 0 + 1, len(s) - 1 - i) + 1):
+        if (s[i - j + 1] == s[i + j]):
+          currentLength += 2
+          currentResult = s[i - j + 1] + currentResult + s[i + j]
+          if currentLength > maxLength:
+            maxLength = currentLength
+            result = currentResult
+        else:
+          break
 
-
-  def isPalindrome(self, s):
-  	if (s == s[::-1]):
-  		return True
-  	else:
-  		return False
+    return result
 
 
 if __name__ == '__main__':
   solution = Solution()
-  print(solution.longestPalindrome('whatabeatifulgirl'))
+  print(solution.longestPalindrome('a'))
