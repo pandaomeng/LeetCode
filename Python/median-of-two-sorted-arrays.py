90db7cc8503e7bd310dd84388e92c472c2ea670a
pandaomeng <pandaomeng@corp-ci.com>
2017-09-15 10:23:07 +0800

    median-of-two-sorted-arrays： 优化算法，log(m+n)

--- Python/median-of-two-sorted-arrays.py
+++ Python/median-of-two-sorted-arrays.py
@@ -1,28 +1,29 @@
 class Solution(object):
-  def findMedianSortedArrays(self, nums1, nums2):
+  def findMedianSortedArrays(self, A, B):
     """
     :type nums1: List[int]
     :type nums2: List[int]
     :rtype: float
     """
-    m = len(nums1)
-    n = len(nums2)
+    m = len(A)
+    n = len(B)
     if m > n:
       A, B, m, n = B, A, n, m
     # 如果half含有.5，则进位（总数为奇数时，左部比右部多一个数）
+    # 分析：因为 i+j 等于half，half又进位了，所以左部比右部多一个数
     half = (m + n + 1) // 2
-    # samll big : 表示nums1的取分割值范围
+    # imin imax : 表示nums1的取分割值范围（个数）
     imin = 0
     imax = m
 
     while imin <= imax:
-      # i j : 两个数组被分割后
+      # i j : 两个数组被分割后的左边的个数
 
       i = (imin + imax) // 2
       j = half - i
       if i < m and B[j - 1] > A[i]:
         # i is too small, must increase it
-      imin = i + 1
+        imin = i + 1
       elif i > 0 and A[i - 1] > B[j]:
         # i is too big, must decrease it
         imax = i - 1
@@ -30,6 +31,7 @@ class Solution(object):
         # i is perfect
         if i == 0:
           max_of_left = B[j - 1]
+        # j 只有在m = n的时候有可能为0
         elif j == 0:
           max_of_left = A[i - 1]
         else:
@@ -50,6 +52,6 @@ class Solution(object):
 
 if __name__ == '__main__':
   solution = Solution()
-  nums1 = [1, 2]
-  nums2 = [3, 4]
+  nums1 = [1, 2, 3, 4, 9]
+  nums2 = [3, 5, 7, 9, 10, 19, 20]
   print(solution.findMedianSortedArrays(nums1, nums2))
