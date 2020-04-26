32b4fb0c0c1465c0fddb88b51a62400e800ea78a
pandaomeng <daomeng.pan@datayes.com>
2020-04-26 22:29:13 +0800

    sliding-window-maximum

---
+++ JS/sliding-window-maximum.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function (nums, k) {
+  const deque = [];
+
+  const pushDeque = (i) => {
+    if (deque[0] === i - k) {
+      deque.shift();
+    }
+    while (deque.length && nums[deque[deque.length - 1]] < nums[i]) {
+      deque.pop();
+    }
+    deque.push(i);
+  };
+
+  let max = nums[0];
+  for (let i = 0; i < k; i++) {
+    pushDeque(i);
+    max = Math.max(max, nums[i]);
+  }
+  const res = [max];
+
+  for (let i = k; i < nums.length; i++) {
+    pushDeque(i);
+    res.push(nums[deque[0]]);
+  }
+
+  return res;
+};
+
+console.log(maxSlidingWindow([1, 3, 1, 2, 0, 5], 3));
