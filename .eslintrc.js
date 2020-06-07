67046b95d1f6616b3ee56bcf5cf1ee4eaa9493b2
pandaomeng <daomeng.pan@datayes.com>
2020-06-07 15:38:23 +0800

    lint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'func-names': 0,
+    'no-unused-vars': 0,
+    'no-console': 0,
+    'no-plusplus': 0
+  },
+};
+
